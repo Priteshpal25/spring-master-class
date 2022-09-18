e82604baac694a7cd9f74477b120cd7a8a5543ac
Pritesh <priteshpal.prp@gmail.com>
2022-09-18 19:52:38 +0530

    component lifecycle methods

--- src/main/java/com/pritechcode/springbasics/springbasics/basics/BinarySearchImpl.java
+++ src/main/java/com/pritechcode/springbasics/springbasics/basics/BinarySearchImpl.java
@@ -1,11 +1,17 @@
 package com.pritechcode.springbasics.springbasics.basics;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearchImpl {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     private SortAlgorithm bubbleSort;
     /* Precedence
@@ -29,4 +35,14 @@ public class BinarySearchImpl {
 		// Search the array
 		return 3;
 	}
+
+    @PostConstruct
+    public void postConstruct(){
+        logger.info("postConstruct");
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        logger.info("preDestroy");
+    }
 }
